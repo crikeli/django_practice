507856cd0d5c1fdcaa82740cf114c9cac085feea
crikeli <christik.dev@gmail.com>
2016-12-24 02:08:00 -0800

    A working implementation of a content creation site

--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def index(request):
     return render(request, 'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content':['If you want to get in touch, feel free to do so.', 'christik.dev@gmail.com']})
